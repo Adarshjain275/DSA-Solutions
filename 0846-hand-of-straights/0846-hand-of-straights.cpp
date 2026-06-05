c93ea5f5c8380b248cf43c18c68706f7ca1af222
Adarsh Jain <adarsh.jain.ug23@nsut.ac.in>
2026-06-05 17:50:23 +0530

    Time: 51 ms (12.03%), Space: 32.2 MB (60.66%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.cpp
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -1,46 +1,33 @@
 class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
-        
-        priority_queue<int> pq;
-        
-        for(int i=0; i<hand.size(); i++)
+
+        int n = hand.size();
+
+        if(n % groupSize != 0)
+            return false;
+
+        map<int,int> freq;
+
+        for(int x : hand)
+            freq[x]++;
+
+        sort(hand.begin(), hand.end());
+
+        for(int card : hand)
         {
-            pq.push(hand[i]);
-        }
+            if(freq[card] == 0)
+                continue;
 
-        while(!pq.empty())
-        {
-            
-            vector<int> temp;
-            int count =0;
-            int curr =-1;
-
-            while(!pq.empty() && count<groupSize)
+            for(int next = card;
+                next < card + groupSize;
+                next++)
             {
+                if(freq[next] == 0)
+                    return false;
 
-            int num = pq.top();
-
-            pq.pop();
-
-            while(!pq.empty() && pq.top()==num)
-            {
-                temp.push_back(pq.top());
-                pq.pop();
+                freq[next]--;
             }
-
-            if(curr==-1) {curr = num; count++;}
-
-            else if(curr!=-1 && curr != num +1) return false;
-
-            else {curr = num; count++;}
-
-            }
-
-            for(int x : temp) pq.push(x);
-
-            if(pq.empty() && count != groupSize ) return false;
-
         }
 
         return true;
