17ac60b85f60a0163945c986a4e9ecd5a3163a7f
Adarsh Jain <adarsh.jain.ug23@nsut.ac.in>
2026-06-08 23:56:19 +0530

    Time: 19 ms (30.36%), Space: 10.2 MB (86.64%) - LeetHub

---
+++ 0457-circular-array-loop/0457-circular-array-loop.cpp
@@ -0,0 +1,54 @@
+class Solution {
+
+public:
+
+        int nextIndex(vector<int>& nums, int curr)
+        {
+            int n = nums.size();
+
+            return ((curr + nums[curr]) % n + n) % n;
+        }
+
+        bool detect(vector<int>& nums, int start)
+        {
+            bool dir = nums[start] > 0;
+
+            int slow = start;
+            int fast = start;
+
+            while(true)
+            {
+                int nextSlow = nextIndex(nums, slow);
+
+                if((nums[slow] > 0) != dir || (nums[nextSlow] > 0) != dir) return false;
+
+                int nextFast = nextIndex(nums, fast);
+
+                if((nums[fast] > 0) != dir || (nums[nextFast] > 0) != dir) return false;
+
+                nextFast = nextIndex(nums, nextFast);
+
+                if((nums[nextFast] > 0) != dir) return false;
+
+                slow = nextSlow;
+                fast = nextFast;
+
+                if(slow == fast)
+                {
+                    if(slow == nextIndex(nums, slow)) return false;   // self-loop
+
+                    return true;
+                }
+            }
+        }
+
+    bool circularArrayLoop(vector<int>& nums) {
+        
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(detect(nums, i)) return true;
+        }
+
+        return false;
+    }
+};
