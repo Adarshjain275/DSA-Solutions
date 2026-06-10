e6096f16d0e96a1ec57e4333fbb9026d52cefcb0
Adarsh Jain <adarsh.jain.ug23@nsut.ac.in>
2026-06-10 16:57:21 +0530

    Time: 0 ms (100%), Space: 8.8 MB (39.95%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,24 +1,37 @@
 class Solution {
-    
-    void getcombs(int k, int n,int index, vector<int>& demo, vector<vector<int>>& ans)
+public:
+
+    void solve(int start,
+               int k,
+               int target,
+               vector<int>& temp,
+               vector<vector<int>>& ans)
     {
-         if(k == 0 && n == 0) {
-            ans.push_back(demo);
+        if(k == 0 && target == 0)
+        {
+            ans.push_back(temp);
             return;
         }
-        if(index > 9 || n < 0 || k < 0) return;
-        
-        demo.push_back(index);
-        getcombs(k-1, n-index, index+1, demo, ans);
-        demo.pop_back();
-        getcombs(k,n, index+1, demo, ans);
-    }
-public:
-    vector<vector<int>> combinationSum3(int k, int n) {
-        vector<vector<int>> ans;
 
-        vector<int> demo;
-        getcombs(k, n,1, demo, ans);
+        if(k == 0 || target < 0)
+            return;
+
+        for(int i=start; i<=9; i++)
+        {
+            temp.push_back(i);
+
+            solve(i+1, k-1, target-i, temp, ans);
+
+            temp.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum3(int k, int n)
+    {
+        vector<vector<int>> ans;
+        vector<int> temp;
+
+        solve(1, k, n, temp, ans);
 
         return ans;
     }
